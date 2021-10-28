91ac49ec507e1ba0ec47ed2a9a4d85ddab9159f0
TakiMoysha <mpwema782@gmail.com>
2021-10-28 21:43:16 +0300

    [conf] change command,there were errors with the global nuitka

--- src/model/configs/consts.py
+++ src/model/configs/consts.py
@@ -5,13 +5,13 @@ from settings import Settings, SettingsAttribute
 settings = Settings()
 
 # PAHT
-SKYRIM_PATH = settings.getValue(SettingsAttribute.skyrim_path)
+# SKYRIM_PATH = settings.getValue(SettingsAttribute.skyrim_path)
 
-SKSE_PATH = path.abspath('/'.join([SKYRIM_PATH, 'skse64_loader.exe']))
+# SKSE_PATH = path.abspath('/'.join([SKYRIM_PATH, 'skse64_loader.exe']))
 
-CLIENT_SETTINGS_PATH = path.abspath(
-    '/'.join([SKYRIM_PATH, 'Platform/Plugins/skymp5-client-settings.txt'])
-)
+# CLIENT_SETTINGS_PATH = path.abspath(
+#     '/'.join([SKYRIM_PATH, 'Platform/Plugins/skymp5-client-settings.txt'])
+# )
 
 # API
 
